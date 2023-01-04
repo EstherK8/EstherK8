13c0d92357126033c3030d45465d94cf5b480166
Esther Katz <estikatz234@gmail.com>
2023-01-03 19:11:53 -0500

    initial commit

---
+++ calculator/interestCalculater.js
@@ -0,0 +1,43 @@
+(function () {
+
+    "use strict";
+
+    let SItotal = (document.getElementById('SItotal'));
+    let CItotal = (document.getElementById('CItotal'));
+
+
+    function SIcalculate() {
+        let principle = (document.getElementById('SIprinciple'));
+        let principleValue = principle.value;
+        let rate = (document.getElementById('SIrate'));
+        let rateValue = rate.value;
+        let time = (document.getElementById('SItime'));
+        let timeValue = time.value;
+        let answer = ((principleValue * (1 + (rateValue * timeValue))));
+        SItotal.innerText = answer;
+    }
+
+    function CIcalculate() {
+        let principle = (document.getElementById('CIprinciple'));
+        let principleValue = principle.value;
+        let rate = (document.getElementById('CIrate'));
+        let rateValue = rate.value;
+        let time = (document.getElementById('CItime'));
+        let timeValue = time.value;
+        let compounds = (document.getElementById('CIcompoundstotal'));
+        let compoundsValue = compounds.value;
+
+
+        //1 can be changed to ntimes per year
+        //formula doesn't work
+        let answer = (principleValue * (Math.pow((1 + (rateValue / compoundsValue)), (timeValue * compoundsValue))));
+        console.log(`answer: ${answer}`);
+        CItotal.innerText = answer;
+    }
+
+    SItotal.addEventListener('click', SIcalculate);
+    CItotal.addEventListener('click', CIcalculate);
+
+})();
+
+
